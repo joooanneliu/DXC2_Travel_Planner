d7f328677b845f8d040110023d2d7f1b51cd2cbc
Jalal-Abdul Yahaya <j.yahaya@wustl.edu>
2024-09-29 17:25:46 -0500

    in progress

--- app.py
+++ app.py
@@ -1,101 +1,88 @@
-from flask import Flask, jsonify, request
+import os
+from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from forms import LoginForm, SignupForm
+from models import db, User
+from flask_migrate import Migrate
 
-app= Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///registration.sqlite3'
-db=SQLAlchemy(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/users.db'  # Adjust to use the instance folder's db
+app.config['SECRET_KEY'] = os.urandom(24)  # Needed for sessions
+db.init_app(app)
 
-@app.route("/")
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'sign_in'  # Redirect users who are not logged in
 
-#CREATING DATABASE
+migrate = Migrate(app, db)
 
-#creating 3 db tables
-class Registration(db.Model):
-    __tablename__= "registrations"
-    userId=db.Column("id",db.Integer, primary_key=True)
-    firstName=db.Column("first_Name", db.String(20))
-    lastName=db.Column("last_Name", db.String(20))
-    email=db.Column("email", db.String(30))
-    password=db.Column("password", db.String(20),unique=True)
-    login= db.relationship("Login", backref="registration")
-    userInformation = db.relationship("User_information", backref="registration")
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-    
-    #requirements for users
-    def __init(self,firstName,lastName,email,password):
-        self.firstName = firstName
-        self.lastName= lastName
-        self.email= email
-        self.password=password
-
-
-
-class Login(db.Model):
-    __tablename__= "login"
-    _id=db.Column("id",db.Integer, primary_key=True)
-    dateCreated= ("date_Created", db.Integer)
-    destination_state= ("destination_state", db.String(10))
-    pdf_link= ("pdf_Link", db.String(30))
-    registration_id= db.Column(db.Integer,db.ForeignKey("registrations.id"))
-
-
-
-    
-    def __init(self, dateCreated, destination_state, pdf_link):
-        self.dateCreated= dateCreated
-        self.destination_state=destination_state
-        self.pdf_link=pdf_link
-
-class User_information(db.Model):
-    __tablename__= "user_information"
-    _id=db.Column("id",db.Integer, primary_key=True)
-    keywords_picked= db.Column("keywords_picked", db.String(30))
-    registration_id= db.Column(db.Integer,db.ForeignKey("registrations.id"))
-
-
-    def __init(self,keywords_picked):
-        self.keywords_picked=keywords_picked
-
+# Creating database tables (if not already existing)
 with app.app_context():
     db.create_all()
 
-#CREATING FLASK PAGES 
+# Flask Routes
 @app.route('/')
-def home():
-    return "This is the homepage"
-@app.route('/sign in', methods=["POST", "GET"])
-def sign_in():
-    if request.method =="POST":
-        username= request.form['username']
-        password= request.form['password']
-        #need a return statement to redirect user to next page which should be flight & car
+def root():
+    return redirect(url_for('sign_in'))
 
-    return "Welcome to the sign in page"
-@app.route('/', methods=["POST", "GET"])
+
+@app.route('/sign_in', methods=["POST", "GET"])
+def sign_in():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('flight_car'))  # Redirect to next page
+        else:
+            flash('Login failed. Check email and password.', 'danger')
+    return render_template('sign-in.html', form=form)
+
+@app.route('/register', methods=["POST", "GET"])
 def register():
-    return "Welcome to the register page"
+    form = SignupForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Account created successfully!', 'success')
+        return redirect(url_for('sign_in'))
+    return render_template('register.html', form=form)
+
 @app.route('/flightandcar')
+@login_required
 def flight_car():
     return "Welcome to the flight and cars page"
+
 @app.route('/hotel')
+@login_required
 def hotel():
     return "Welcome to the hotel page"
+
 @app.route('/itinerary')
+@login_required
 def itinerary():
     return "Welcome to the itinerary"
+
 @app.route('/confirmation')
+@login_required
 def confirmation():
-    return "Welcome to the confirmation page Confirmation page"
+    return "Welcome to the confirmation page"
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('sign_in'))
 
-
-
-
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
-    
-    
-
-#f.close()   
--- models.py
+++ models.py
@@ -4,7 +4,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
+#User table for login and registration
 class User(db.Model, UserMixin):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
@@ -15,3 +17,44 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+#Registration table
+class Registration(db.Model):
+    __tablename__ = "registrations"
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)  # Storing hashed password
+    login = db.relationship("Login", backref="registration", uselist=False)
+    user_information = db.relationship("UserInformation", backref="registration", uselist=False)
+
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = generate_password_hash(password)  # Hash the password
+
+#Login table
+class Login(db.Model):
+    __tablename__ = "login"
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.Integer)
+    destination_state = db.Column(db.String(10))
+    pdf_link = db.Column(db.String(30))
+    registration_id = db.Column(db.Integer, db.ForeignKey("registrations.id"))
+
+    def __init__(self, date_created, destination_state, pdf_link):
+        self.date_created = date_created
+        self.destination_state = destination_state
+        self.pdf_link = pdf_link
+
+#UserInformation table
+class UserInformation(db.Model):
+    __tablename__ = "user_information"
+    id = db.Column(db.Integer, primary_key=True)
+    keywords_picked = db.Column(db.String(30))
+    registration_id = db.Column(db.Integer, db.ForeignKey("registrations.id"))
+
+    def __init__(self, keywords_picked):
+        self.keywords_picked = keywords_picked

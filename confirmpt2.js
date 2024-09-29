703c49ceff035b6669e37907cf5299efc14990eb
Lisa Farley <username@swarthmore.edu>
2024-09-29 17:53:02 -0400

    added html, css, and js

--- confirmpt2.js
+++ confirmpt2.js
@@ -3,13 +3,13 @@ function addPerson() {
     const select = document.createElement('select');
 
     select.innerHTML = `
-        <option value="0-5">0 - 5</option>
-        <option value="6-12">6 - 12</option>
-        <option value="13-17">13 - 17</option>
-        <option value="18-20">18 - 20</option>
-        <option value="21-35">21 - 35</option>
-        <option value="36-59">36 - 59</option>
-        <option value="60+">60+</option>
+        <option value="0-5 years">0 - 5 years</option>
+        <option value="6-12 years">6 - 12 years</option>
+        <option value="13-17 years">13 - 17 years</option>
+        <option value="18-20 years">18 - 20 years</option>
+        <option value="21-35 years">21 - 35 years</option>
+        <option value="36-59 years">36 - 59 years</option>
+        <option value="60+ years">60+ years</option>
     `;
 
     peopleContainer.insertBefore(select, peopleContainer.lastElementChild);

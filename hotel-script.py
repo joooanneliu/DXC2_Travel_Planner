973f2218e2cc3ff5bd84141d5a73b8195a34d2df
Joanne <joannechanliu@gmail.com>
2024-09-29 19:24:51 -0500

    Tested user input and finished script to extra hotel info from json

--- hotel-script.py
+++ hotel-script.py
@@ -2,6 +2,6 @@ import json
 with open('hotel-sample.json', 'r') as file:
     data = json.load(file)
 
-hotel_names = [property['name'] for property in data['properties'] if property['type'] == 'hotel']
+hotel_names = [property['name'] + " " + property['total_rate']['lowest'] + ' ' + property['hotel_class'][0:6] for property in data['properties'] if property['type'] == 'hotel']
 
 print(hotel_names)
